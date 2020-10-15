b516d740414b0e7e783e9e0267739a2a39b54fb4
Miguel Avila <miguelaaa89@gmail.com>
2020-10-15 18:41:38 -0500

    Docs agregado

---
+++ docs/precache-manifest.313334ce557e0e356fb2be28eef53dbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03fae89a3b2a01bdb04e6e49481e6c09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dafa9b37d0a78f01114",
+    "url": "/static/css/main.52e5179e.chunk.css"
+  },
+  {
+    "revision": "080c2afeafe12f9f07ad",
+    "url": "/static/js/2.fc4b439f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fc4b439f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dafa9b37d0a78f01114",
+    "url": "/static/js/main.27be8e00.chunk.js"
+  },
+  {
+    "revision": "72e4b1f2ff4246389bc8",
+    "url": "/static/js/runtime-main.a50afc49.js"
+  }
+]);
